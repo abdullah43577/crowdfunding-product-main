d72a6740e8ecd1a09446d077d59ae3fca9694883
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-02-08 22:06:04 +0100

    finished working on the UI interface and responsiveness

--- app/script.js
+++ app/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const backProject = document.querySelector(".backProject");
+const overlay = document.querySelector(".overlay");
+const modalStart = document.getElementById("modal--start");
+const modalClose = document.querySelector(".modal--close");
+const burger = document.querySelector(".burger");
+
+const showModal = function () {
+  overlay.classList.remove("hide");
+  modalStart.classList.remove("hide");
+};
+
+const closeModal = function () {
+  overlay.classList.add("hide");
+  modalStart.classList.add("hide");
+};
+
+backProject.addEventListener("click", showModal);
+modalClose.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// burger menu
+const showMenu = document.querySelector(".show");
+const overlay2 = document.querySelector(".overlay2");
+burger.addEventListener("click", function () {
+  console.log("clicked");
+  burger.classList.toggle("toggle");
+  showMenu.classList.toggle("translate-x-0");
+  overlay2.classList.toggle("hidden");
+});
